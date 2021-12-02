e6e496b8007a6cf19a37ac9af8154570354f3864
MatthewZid <m_zidianakis@hotmail.com>
2021-12-02 13:46:49 +0200

    added posterior net and discriminator training

--- 2dnet.py
+++ 2dnet.py
@@ -29,6 +29,9 @@ class CircleAgent():
         self.generated_actions = []
         self.expert_states = []
         self.expert_actions = []
+        self.sampled_expert_states = []
+        self.sampled_expert_actions = []
+        self.disc_optimizer = tf.keras.optimizers.RMSprop()
         print('\nAgent created')
 
     def create_generator(self, initializer):
@@ -47,6 +50,20 @@ class CircleAgent():
         return model
 
     def create_discriminator(self, initializer):
+        states = Input(shape=self.state_dims)
+        actions = Input(shape=self.action_dims)
+        merged = tf.concat([states,actions], 1)
+        # x = Flatten()(merged)
+        x = Dense(128, kernel_initializer=initializer)(merged)
+        x = ReLU()(x)
+        x = Dense(128, kernel_initializer=initializer)(x)
+        x = ReLU()(x)
+        score = Dense(1)(x)
+
+        model = Model(inputs=[states, actions], outputs=score)
+        return model
+    
+    def create_posterior(self, initializer):
         states = Input(shape=self.state_dims)
         actions = Input(shape=self.action_dims)
         merged = tf.concat([states,actions], 1)
@@ -96,12 +113,31 @@ class CircleAgent():
         
         return (s_traj, a_traj)
     
+    def __disc_loss(self, expert_score, gen_score):
+        cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+        
+        expert_loss = cross_entropy(tf.ones_like(expert_score), expert_score)
+        gen_loss = cross_entropy(tf.zeros_like(gen_score), gen_score)
+        total_loss = expert_loss + gen_loss
+
+        return total_loss
+    
     def view_traj(self, traj):
         plt.scatter(traj[:,-2], traj[:,-1], c=['red'], alpha=0.4)
         plt.show()
 
     def train(self):
-        pass
+        # train discriminator
+        with tf.GradientTape() as disc_tape:
+            expert_score = self.discriminator([self.expert_states, self.expert_actions], training=True)
+            generated_score = self.discriminator([self.generated_states, self.generated_actions], training=True)
+
+            disc_loss = self.__disc_loss(expert_score, generated_score)
+        
+        gradients_of_discriminator = disc_tape.gradient(disc_loss, self.discriminator.trainable_variables)
+        self.disc_optimizer.apply_gradients(zip(gradients_of_discriminator, self.discriminator.trainable_variables))
+
+        # train posterior
     
     def infogail(self):
         # load data
@@ -109,15 +145,8 @@ class CircleAgent():
 
         self.expert_states = np.concatenate(self.expert_states)
         self.expert_actions = np.concatenate(self.expert_actions)
-        self.expert_states = tf.convert_to_tensor(self.expert_states, dtype=tf.float32)
-        self.expert_actions = tf.convert_to_tensor(self.expert_actions, dtype=tf.float32)
 
-        self.expert_states = tf.data.Dataset.from_tensor_slices(self.expert_states)
-        self.expert_states = self.expert_states.shuffle(buffer_size=5000).batch(self.batch)
-        self.expert_actions = tf.data.Dataset.from_tensor_slices(self.expert_actions)
-        self.expert_actions = self.expert_actions.shuffle(buffer_size=5000).batch(self.batch)
-
-        # place an epoch loop here
+        # probably place an epoch loop here
 
         # Sample a batch of latent codes: ci ∼ p(c)
         sampled_codes = np.zeros((self.batch, self.code_dims))
@@ -144,17 +173,30 @@ class CircleAgent():
         self.generated_actions = np.concatenate(self.generated_actions)
 
         # Sample state-action pairs χi ~ τi and χΕ ~ τΕ with the same batch size
-        # generated_idx = np.random.choice(generated_states.shape[0], self.batch, replace=False)
-        # expert_idx = np.random.choice(expert_states.shape[0], self.batch, replace=False)
+        print("\nSampling state-action pairs...")
+        generated_idx = np.random.choice(self.generated_states.shape[0], self.batch, replace=False)
+        expert_idx = np.random.choice(self.expert_states.shape[0], self.batch, replace=False)
+
+        self.generated_states = self.generated_states[generated_idx, :]
+        self.generated_actions = self.generated_actions[generated_idx, :]
+        self.sampled_expert_states = self.expert_states[expert_idx, :]
+        self.sampled_expert_actions = self.expert_actions[expert_idx, :]
+
         print("\nCreating dataset...")
-        # probably wrong, sample batch first like above in comments and create the dataset with the batch
         self.generated_states = tf.convert_to_tensor(self.generated_states, dtype=tf.float32)
         self.generated_actions = tf.convert_to_tensor(self.generated_actions, dtype=tf.float32)
+        self.sampled_expert_states = tf.convert_to_tensor(self.sampled_expert_states, dtype=tf.float32)
+        self.sampled_expert_actions = tf.convert_to_tensor(self.sampled_expert_actions, dtype=tf.float32)
 
         self.generated_states = tf.data.Dataset.from_tensor_slices(self.generated_states)
-        self.generated_states = self.generated_states.shuffle(buffer_size=10000).batch(self.batch)
+        self.generated_states = self.generated_states.shuffle(buffer_size=100).batch(batch_size=self.batch)
         self.generated_actions = tf.data.Dataset.from_tensor_slices(self.generated_actions)
-        self.generated_actions = self.generated_actions.shuffle(buffer_size=10000).batch(self.batch)
+        self.generated_actions = self.generated_actions.shuffle(buffer_size=100).batch(batch_size=self.batch)
+
+        self.sampled_expert_states = tf.data.Dataset.from_tensor_slices(self.sampled_expert_states)
+        self.sampled_expert_states = self.sampled_expert_states.shuffle(buffer_size=100).batch(batch_size=self.batch)
+        self.sampled_expert_actions = tf.data.Dataset.from_tensor_slices(self.sampled_expert_actions)
+        self.sampled_expert_actions = self.sampled_expert_actions.shuffle(buffer_size=100).batch(batch_size=self.batch)
 
         # call train here
 
