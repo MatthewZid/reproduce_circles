06ed12f8436d61822c41437f4fcf9390cc80217a
MatthewZid <m_zidianakis@hotmail.com>
2022-02-03 05:27:51 +0200

    fixed training and generator testing

--- behavioural_cloning.py
+++ behavioural_cloning.py
@@ -9,22 +9,20 @@ import pickle as pkl
 import matplotlib.pyplot as plt
 from trpo import *
 from tqdm import trange
-from circle_env import CircleEnv
 
 BATCH_SIZE = 2048
-show_fig = False
-show_traj = True
+show_fig = True
 
 def create_generator(state_dims, code_dims):
-    initializer = tf.keras.initializers.GlorotUniform()
+    initializer = tf.keras.initializers.RandomNormal()
     states = Input(shape=state_dims)
-    x = Dense(128, activation='tanh', kernel_initializer=initializer)(states)
-    # x = ReLU()(x)
-    x = Dense(128, activation='tanh', kernel_initializer=initializer)(x)
-    # x = ReLU()(x)
+    x = Dense(128, kernel_initializer=initializer)(states)
+    x = LeakyReLU()(x)
+    x = Dense(128, kernel_initializer=initializer)(x)
+    x = LeakyReLU()(x)
     codes = Input(shape=code_dims)
-    c = Dense(128, activation='tanh', kernel_initializer=initializer)(codes)
-    # c = ReLU()(c)
+    c = Dense(128, kernel_initializer=initializer)(codes)
+    c = LeakyReLU()(c)
     h = Add()([x, c])
     # h = tf.concat([x,c], 1)
     actions = Dense(2, activation='tanh')(h)
@@ -36,51 +34,8 @@ def standardize(states):
     for i in range(states.shape[1]):
         states[:,i] = (states[:,i] - states[:,i].mean()) / states[:,i].std()
 
-env = CircleEnv(max_step=256)
-
-def generate_policy(generator, code):
-    s_traj = []
-    a_traj = []
-    c_traj = []
-
-    # logstd = np.array([0.0,0.0])
-
-    # generate actions for every current state
-    state_obsrv = env.reset() # reset environment state
-    code_tf = tf.constant(code)
-    code_tf = tf.expand_dims(code_tf, axis=0)
-
-    while True:
-        # 1. generate actions with generator
-        state_tf = tf.constant(state_obsrv)
-        state_tf = tf.expand_dims(state_tf, axis=0)
-        action = generator([state_tf, code_tf], training=False)
-        action = tf.squeeze(action).numpy()
-        # action_std = np.exp(logstd)
-
-        # sample action
-        # z = np.random.randn(1, logstd.shape[0])
-        # action = action_mu + action_std * z[0]
-        # action = np.clip(action, -1, 1)
-
-        # current_state = (state_obsrv[-2], state_obsrv[-1])
-        s_traj.append(state_obsrv)
-        a_traj.append(action)
-        c_traj.append(code)
-
-        # 2. environment step
-        state_obsrv, done = env.step(action)
-
-        if done:
-            s_traj = np.array(s_traj, dtype=np.float32)
-            a_traj = np.array(a_traj, dtype=np.float32)
-            c_traj = np.array(c_traj, dtype=np.float32)
-            break
-
-    return (s_traj, a_traj, c_traj)
-
 # load data
-expert_states, expert_actions, expert_codes, code_prob = pkl.load(open("expert_traj.pkl", "rb"))
+expert_states, expert_actions, expert_codes = pkl.load(open("expert_traj.pkl", "rb"))
 
 expert_states = np.concatenate(expert_states)
 expert_actions = np.concatenate(expert_actions)
@@ -101,8 +56,6 @@ val_actions = shuffled_expert_actions[train_ratio:, :]
 train_codes = shuffled_expert_codes[0:train_ratio, :]
 val_codes = shuffled_expert_codes[train_ratio:, :]
 
-standardize(train_states)
-
 train_states = tf.convert_to_tensor(train_states, dtype=tf.float32)
 train_actions = tf.convert_to_tensor(train_actions, dtype=tf.float32)
 train_codes = tf.convert_to_tensor(train_codes, dtype=tf.float32)
@@ -119,11 +72,12 @@ val_data = val_data.batch(batch_size=BATCH_SIZE)
 # train
 generator = create_generator(10, 3)
 
-gen_optimizer = tf.keras.optimizers.SGD(learning_rate=1e-2)
+# gen_optimizer = tf.keras.optimizers.SGD(learning_rate=1e-3)
+gen_optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
 mse = tf.keras.losses.MeanSquaredError()
-# actions_logstd = np.zeros((BATCH_SIZE, expert_actions.shape[1]), dtype=np.float32)
 
-epochs = 50
+# epochs = 50
+epochs = 500
 total_train_size = sum([el[0].shape[0] for el in list(train_data.as_numpy_iterator())])
 total_val_size = sum([el[0].shape[0] for el in list(val_data.as_numpy_iterator())])
 result_train = []
@@ -133,9 +87,6 @@ for epoch in trange(epochs, desc='Epoch'):
     for _, (states_batch, actions_batch, codes_batch) in enumerate(train_data):
         with tf.GradientTape() as gen_tape:
             actions_mu = generator([states_batch, codes_batch], training=True)
-            # z = tf.random.normal([BATCH_SIZE,2], 0, 0.4)
-            # actions = actions_mu + tf.math.exp(actions_logstd) * z
-            # actions = np.clip(actions, -1, 1)
             gen_loss = mse(actions_batch, actions_mu)
         
         policy_gradients = gen_tape.gradient(gen_loss, generator.trainable_weights)
@@ -155,18 +106,6 @@ for epoch in trange(epochs, desc='Epoch'):
     epoch_loss = loss / total_val_size
     result_val.append(epoch_loss)
 
-    # generate trajectory
-    if show_traj:
-        colors = ['red','green','blue']
-        plt.figure()
-        codes = np.zeros((3, 3))
-        for i in range(3):
-            codes[i,i] = 1
-            traj = generate_policy(generator, codes[i])
-            plt.scatter(traj[0][:, -2], traj[0][:, -1], c=colors[i], alpha=0.4)
-        plt.savefig("./plots/trajectories_"+str(epoch), dpi=100)
-        plt.close()
-
 if show_fig:
     epoch_space = np.arange(1, len(result_train)+1, dtype=int)
     plt.figure()
@@ -178,4 +117,4 @@ if show_fig:
     plt.close()
 
 generator.save_weights('./saved_models/generator.h5')
-print('\nModels saved!')
+print('\nGenerator saved!')
---
+++ test_generator.py
@@ -0,0 +1,85 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Dense, ReLU, LeakyReLU, Add
+from tensorflow.keras.models import Model
+import numpy as np
+import pickle as pkl
+import matplotlib.pyplot as plt
+from trpo import *
+from tqdm import trange
+from circle_env import CircleEnv
+
+env = CircleEnv()
+
+def create_generator(state_dims, code_dims):
+    initializer = tf.keras.initializers.RandomNormal()
+    states = Input(shape=state_dims)
+    x = Dense(128, kernel_initializer=initializer)(states)
+    x = LeakyReLU()(x)
+    x = Dense(128, kernel_initializer=initializer)(x)
+    x = LeakyReLU()(x)
+    codes = Input(shape=code_dims)
+    c = Dense(128, kernel_initializer=initializer)(codes)
+    c = LeakyReLU()(c)
+    h = Add()([x, c])
+    # h = tf.concat([x,c], 1)
+    actions = Dense(2, activation='tanh')(h)
+
+    model = Model(inputs=[states,codes], outputs=actions)
+    return model
+
+def generate_policy(generator, code, starting_point):
+    s_traj = []
+    a_traj = []
+    c_traj = []
+
+    # generate actions for every current state
+    state_obsrv = env.reset(start=starting_point) # reset environment state
+    code_tf = tf.constant(code)
+    code_tf = tf.expand_dims(code_tf, axis=0)
+
+    while True:
+        # 1. generate actions with generator
+        state_tf = tf.constant(state_obsrv)
+        state_tf = tf.expand_dims(state_tf, axis=0)
+        action = generator([state_tf, code_tf], training=False)
+        action = tf.squeeze(action).numpy()
+
+        # current_state = (state_obsrv[-2], state_obsrv[-1])
+        s_traj.append(state_obsrv)
+        a_traj.append(action)
+        c_traj.append(code)
+
+        # 2. environment step
+        state_obsrv, done = env.step(action)
+
+        if done:
+            s_traj = np.array(s_traj, dtype=np.float32)
+            a_traj = np.array(a_traj, dtype=np.float32)
+            c_traj = np.array(c_traj, dtype=np.float32)
+            break
+
+    return (s_traj, a_traj, c_traj)
+
+generator = create_generator(10, 3)
+generator.load_weights('./saved_models/generator.h5')
+
+expert_states, expert_actions, expert_codes = pkl.load(open("expert_traj.pkl", "rb"))
+
+expert_states = np.concatenate(expert_states)
+expert_actions = np.concatenate(expert_actions)
+expert_codes = np.concatenate(expert_codes)
+
+colors = ['red','green','blue']
+plt.figure()
+plt.xlim(-1,1)
+plt.ylim(-1,1)
+for i in trange(8):
+    pick = np.random.choice(expert_codes.shape[0], 1)[0]
+    traj = generate_policy(generator, expert_codes[pick], (expert_states[pick, -2], expert_states[pick, -1]))
+    argcolor = np.where(expert_codes[pick] == 1)[0][0] # find the index of code from one-hot
+    plt.scatter(traj[0][:, -2], traj[0][:, -1], s=4, c=colors[argcolor], alpha=0.4)
+plt.savefig("./plots/generated_trajectories", dpi=100)
+plt.close()
