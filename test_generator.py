d9c6eb8c55ceb293c11057c04ed9bf93da604057
MatthewZid <m_zidianakis@hotmail.com>
2022-02-17 05:58:32 +0200

    minor change

--- test_generator.py
+++ test_generator.py
@@ -1,9 +1,10 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Dense, ReLU, LeakyReLU, Add
-from tensorflow.keras.models import Model
+from tensorflow.python.keras.layers import Input, Dense, ReLU, LeakyReLU, Add
+from tensorflow.python.keras.models import Model
 import numpy as np
 import pickle as pkl
 import matplotlib.pyplot as plt
