fb440ef436f347ae4c45158d1e6c170af96b95ee
MatthewZid <m_zidianakis@hotmail.com>
2022-03-22 23:12:19 +0200

    structure reconstruction

---
+++ infogail.py
@@ -0,0 +1,297 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import pickle as pkl
+import numpy as np
+import yaml
+
+from circle_env import CircleEnv
+from tqdm import trange
+from trpo import *
+import trpo
+from scipy.ndimage import shift
+import multiprocessing as mp
+from models import *
+
+# tf.config.threading.set_inter_op_parallelism_threads(16) 
+# tf.config.threading.set_intra_op_parallelism_threads(16)
+# tf.config.set_soft_device_placement(True)
+
+class Agent():
+    def __generate_trajectory(self, code):
+        s_traj = []
+        a_traj = []
+        c_traj = []
+        env = CircleEnv()
+
+        logstd = np.array([LOGSTD, LOGSTD])
+
+        # generate actions for every current state
+        state_obsrv = env.reset() # reset environment state
+        code_tf = tf.constant(code)
+        code_tf = tf.expand_dims(code_tf, axis=0)
+
+        while True:
+            # 1. generate actions with generator
+            state_tf = tf.constant(state_obsrv)
+            state_tf = tf.expand_dims(state_tf, axis=0)
+            action_mu = models.generator.model([state_tf, code_tf], training=False)
+            action_mu = tf.squeeze(action_mu).numpy()
+            action_std = np.exp(logstd)
+
+            # sample action
+            z = np.random.randn(1, logstd.shape[0])
+            action = action_mu + action_std * z[0]
+            # action = np.clip(action, -1, 1)
+
+            # current_state = (state_obsrv[-2], state_obsrv[-1])
+            s_traj.append(state_obsrv)
+            a_traj.append(action)
+            c_traj.append(code)
+
+            # 2. environment step
+            state_obsrv, done = env.step(action)
+
+            if done:
+                s_traj = np.array(s_traj, dtype=np.float32)
+                a_traj = np.array(a_traj, dtype=np.float32)
+                c_traj = np.array(c_traj, dtype=np.float32)
+                break
+        
+        return (s_traj, a_traj, c_traj)
+    
+    def run(self, code):
+        trajectory_dict = {}
+        trajectory = self.__generate_trajectory(code)
+        trajectory_dict['states'] = np.copy(trajectory[0])
+        trajectory_dict['actions'] = np.copy(trajectory[1])
+        trajectory_dict['codes'] = np.copy(trajectory[2])
+        return trajectory_dict
+
+class InfoGAIL():
+    def __init__(self, batch_size=2048, code_batch=384, episodes=6000, gamma=0.997, lam=0.97):
+        self.batch = batch_size
+        self.code_batch = code_batch
+        self.episodes = episodes
+        self.gamma = gamma
+        self.lam = lam
+        self.starting_episode = 0
+        self.gen_result = []
+        self.disc_result = []
+        self.post_result = []
+        self.value_result = []
+        self.total_rewards = []
+
+        # load data
+        expert_states, expert_actions, expert_codes = pkl.load(open("expert_traj.pkl", "rb"))
+
+        self.expert_states = np.concatenate(expert_states)
+        self.expert_actions = np.concatenate(expert_actions)
+        self.expert_codes = np.concatenate(expert_codes)
+
+        generator_weight_path = ''
+        if resume_training:
+            with open("./saved_models/trpo/model.yml", 'r') as f:
+                data = yaml.safe_load(f)
+                self.starting_episode = data['episode']
+                self.gen_result = data['gen_loss']
+                self.disc_result = data['disc_loss']
+                self.post_result = data['post_loss']
+                self.value_result = data['value_loss']
+            generator_weight_path = './saved_models/trpo/generator.h5'
+            models.discriminator.model.load_weights('./saved_models/trpo/discriminator.h5')
+            models.posterior.model.load_weights('./saved_models/trpo/posterior.h5')
+            models.value_net.model.load_weights('./saved_models/trpo/value_net.h5')
+        else:
+            generator_weight_path = './saved_models/bc/generator.h5'
+        
+        models.generator.model.load_weights(generator_weight_path)
+        print('\nSetup ready!')
+    
+    def __saveplot(self, x, y, episode, element='element', mode='plot'):
+        plt.figure()
+        if mode == 'plot': plt.plot(x, y)
+        else: plt.scatter(x, y, alpha=0.4)
+        plt.savefig('./plots/'+element+'_'+str(episode), dpi=100)
+        plt.close()
+    
+    def normalize(self, states):
+        for i in range(states.shape[1]):
+            states[:,i] = (states[:,i] - states[:,i].min()) / (states[:,i].max() - states[:,i].min())
+
+    def standardize(self, states):
+        for i in range(states.shape[1]):
+            states[:,i] = (states[:,i] - states[:,i].mean()) / states[:,i].std()
+    
+    def show_loss(self):
+        epoch_space = np.arange(1, len(self.gen_result)+1, dtype=int)
+        plt.figure()
+        plt.ylim(-100, 100)
+        plt.title('Surrogate loss')
+        plt.plot(epoch_space, self.gen_result)
+        plt.savefig('./plots/trpo_loss', dpi=100)
+        plt.close()
+
+        plt.figure()
+        plt.title('Disc/Post losses')
+        plt.plot(epoch_space, self.disc_result)
+        plt.plot(epoch_space, self.post_result)
+        plt.legend(['disc', 'post'], loc="lower left")
+        plt.savefig('./plots/disc_post', dpi=100)
+        plt.close()
+
+        plt.figure()
+        plt.title('Value loss')
+        plt.plot(epoch_space, self.value_result)
+        plt.savefig('./plots/value_loss', dpi=100)
+        plt.close()
+    
+    def train(self, agent):
+        for episode in trange(self.starting_episode, self.episodes, desc="Episode"):
+            # Sample a batch of latent codes: ci ∼ p(c)
+            sampled_codes = np.zeros((self.code_batch, models.generator.code_dims))
+            for i in range(self.code_batch):
+                pick = np.random.choice(models.generator.code_dims, 1)[0]
+                sampled_codes[i, pick] = 1
+            
+            # Sample trajectories: τi ∼ πθi(ci), with the latent code fixed during each rollout
+            trajectories = []
+            with mp.Pool(mp.cpu_count()) as pool:
+                trajectories = pool.map(agent.run, sampled_codes)
+            
+            for traj in trajectories:
+                traj['old_action_mus'] = models.generator.model([traj['states'], traj['codes']], training=False)
+            
+            generated_states = np.concatenate([traj['states'] for traj in trajectories])
+            generated_actions = np.concatenate([traj['actions'] for traj in trajectories])
+            generated_codes = np.concatenate([traj['codes'] for traj in trajectories])
+            generated_oldactions = np.concatenate([traj['old_action_mus'] for traj in trajectories])
+
+            # train discriminator
+            # Sample state-action pairs χi ~ τi and χΕ ~ τΕ with the same batch size
+            expert_idx = np.arange(self.expert_states.shape[0])
+            np.random.shuffle(expert_idx)
+            shuffled_expert_states = self.expert_states[expert_idx, :]
+            shuffled_expert_actions = self.expert_actions[expert_idx, :]
+
+            generated_idx = np.arange(generated_states.shape[0])
+            np.random.shuffle(generated_idx)
+
+            shuffled_generated_states = generated_states[generated_idx, :]
+            shuffled_generated_actions = generated_actions[generated_idx, :]
+            shuffled_generated_states = tf.convert_to_tensor(shuffled_generated_states, dtype=tf.float32)
+            shuffled_generated_actions = tf.convert_to_tensor(shuffled_generated_actions, dtype=tf.float32)
+            shuffled_expert_states = tf.convert_to_tensor(shuffled_expert_states, dtype=tf.float32)
+            shuffled_expert_actions = tf.convert_to_tensor(shuffled_expert_actions, dtype=tf.float32)
+
+            dataset = tf.data.Dataset.from_tensor_slices((shuffled_generated_states, shuffled_generated_actions, shuffled_expert_states, shuffled_expert_actions))
+            dataset = dataset.batch(batch_size=self.batch)
+
+            loss = models.discriminator.train(dataset)
+            if save_loss: self.disc_result.append(loss)
+
+            # train posterior
+            generated_idx = np.arange(generated_states.shape[0])
+            np.random.shuffle(generated_idx)
+            shuffled_generated_states = generated_states[generated_idx, :]
+            shuffled_generated_actions = generated_actions[generated_idx, :]
+            shuffled_generated_codes = generated_codes[generated_idx, :]
+            shuffled_generated_states = tf.convert_to_tensor(shuffled_generated_states, dtype=tf.float32)
+            shuffled_generated_actions = tf.convert_to_tensor(shuffled_generated_actions, dtype=tf.float32)
+            shuffled_generated_codes = tf.convert_to_tensor(shuffled_generated_codes, dtype=tf.float32)
+            dataset = tf.data.Dataset.from_tensor_slices((shuffled_generated_states, shuffled_generated_actions, shuffled_generated_codes))
+            dataset = dataset.batch(batch_size=self.batch)
+
+            loss = models.posterior.train(dataset)
+            if save_loss: self.post_result.append(loss)
+
+            # TRPO/PPO
+            # calculate rewards from discriminator and posterior
+            episode_rewards = []
+            for traj in trajectories:
+                reward_d = (-tf.math.log(tf.keras.activations.sigmoid(models.discriminator.model([traj['states'], traj['actions']], training=False)))).numpy()
+                reward_p = models.posterior.model([traj['states'], traj['actions']], training=False).numpy()
+                reward_p = np.sum(np.ma.log(reward_p).filled(0) * traj['codes'], axis=1).flatten() # np.ma.log over tf.math.log, fixes log of zero
+
+                traj['rewards'] = 0.6 * reward_d.flatten() + 0.4 * reward_p
+                episode_rewards.append(traj['rewards'].sum())
+
+                # calculate values, advants and returns
+                values = models.value_net.model([traj['states'], traj['codes']], training=False).numpy().flatten() # Value function
+                values_next = shift(values, -1, cval=0)
+                deltas = traj['rewards'] + self.gamma * values_next - values # Advantage(st,at) = rt+1 + γ*V(st+1) - V(st)
+                traj['advants'] = discount(deltas, self.gamma * self.lam)
+                traj['returns'] = discount(traj['rewards'], self.gamma)
+            
+            advants = np.concatenate([traj['advants'] for traj in trajectories])
+            advants = (advants - advants.mean()) / advants.std()
+
+            # train value net for next iter
+            returns = np.expand_dims(np.concatenate([traj['returns'] for traj in trajectories]), axis=1)
+
+            generated_idx = np.arange(generated_states.shape[0])
+            np.random.shuffle(generated_idx)
+            shuffled_generated_states = generated_states[generated_idx, :]
+            shuffled_generated_codes = generated_codes[generated_idx, :]
+            shuffled_returns = returns[generated_idx, :]
+            shuffled_generated_states = tf.convert_to_tensor(shuffled_generated_states, dtype=tf.float32)
+            shuffled_generated_codes = tf.convert_to_tensor(shuffled_generated_codes, dtype=tf.float32)
+            shuffled_returns = tf.convert_to_tensor(shuffled_returns, dtype=tf.float32)
+            dataset = tf.data.Dataset.from_tensor_slices((shuffled_generated_states, shuffled_generated_codes, shuffled_returns))
+            dataset = dataset.batch(batch_size=self.batch)
+
+            loss = models.value_net.train(dataset)
+            if save_loss: self.value_result.append(loss)
+
+            # generator training
+            feed = {
+                'states': generated_states,
+                'actions': generated_actions,
+                'codes': generated_codes,
+                'advants': advants,
+                'old_mus': generated_oldactions
+            } 
+
+            loss = models.generator.train(feed)
+            if save_loss: self.gen_result.append(loss)
+
+            if save_loss:
+                # plot rewards and losses
+                episode_reward = np.array(episode_rewards, dtype=np.float32).mean()
+                self.total_rewards.append(episode_reward)
+                if episode != 0:
+                    self.show_loss()
+                    epoch_space = np.arange(1, len(self.total_rewards)+1, dtype=int)
+                    self.__saveplot(epoch_space, self.total_rewards, 0, 'rewards')
+            
+            if episode != 0 and (episode % 100 == 0): print('Theta updates so far: {:d}'.format(trpo.improved))
+
+            if save_models:
+                models.generator.model.save_weights('./saved_models/trpo/generator.h5')
+                models.discriminator.model.save_weights('./saved_models/trpo/discriminator.h5')
+                models.posterior.model.save_weights('./saved_models/trpo/posterior.h5')
+                models.value_net.model.save_weights('./saved_models/trpo/value_net.h5')
+                yaml_conf = {
+                    'episode': episode+1,
+                    'gen_loss': self.gen_result,
+                    'disc_loss': self.disc_result,
+                    'post_loss': self.post_result,
+                    'value_loss': self.value_result
+                }
+                
+                with open("./saved_models/trpo/model.yml", 'w') as f:
+                    yaml.dump(yaml_conf, f, sort_keys=False, default_flow_style=False)
+
+models = Models()
+
+# main
+def main():
+    agent = Agent()
+    infogail = InfoGAIL()
+    infogail.train(agent)
+
+if __name__ == '__main__':
+    main()
---
+++ models.py
@@ -0,0 +1,313 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+
+import tensorflow as tf
+import tensorflow_probability as tfp
+from tensorflow.python.keras.layers import Input, Dense, LeakyReLU, Add
+from tensorflow.python.keras.models import Model
+import numpy as np
+from trpo import *
+
+tfd = tfp.distributions
+
+class Generator():
+    def __init__(self, state_dims, action_dims, code_dims, epsilon=0.2, max_kl=0.01):
+        self.state_dims = state_dims
+        self.action_dims = action_dims
+        self.code_dims = code_dims
+        self.epsilon = epsilon
+        self.max_kl = max_kl
+        self.model = self.create_generator()
+    
+    def create_generator(self):
+        initializer = tf.keras.initializers.RandomNormal()
+        states = Input(shape=self.state_dims)
+        x = Dense(100, kernel_initializer=initializer)(states)
+        x = LeakyReLU()(x)
+        codes = Input(shape=self.code_dims)
+        c = Dense(64, kernel_initializer=initializer)(codes)
+        c = LeakyReLU()(c)
+        # h = Add()([x, c])
+        h = tf.concat([x,c], 1)
+        actions = Dense(self.action_dims)(h)
+
+        model = Model(inputs=[states,codes], outputs=actions)
+        return model
+
+    def __generator_loss(self, feed):
+        # calculate ratio between old and new policy (surrogate loss)
+        with tf.GradientTape(persistent=True) as grad_tape:
+            actions_mu = self.model([feed['states'], feed['codes']], training=True)
+
+            nans = tf.math.is_nan(actions_mu)
+            if(tf.where(nans).numpy().flatten().shape[0] != 0): print('Mus: NAN!!!!!!!!!!!')
+
+            # log_p_n = gauss_log_prob(actions_mu, LOGSTD, feed['actions'])
+            # log_oldp_n = gauss_log_prob(feed['old_actions'], LOGSTD, feed['actions'])
+            # ...OR...
+            dist = tfd.MultivariateNormalDiag(loc=actions_mu, scale_diag=[tf.exp(LOGSTD), tf.exp(LOGSTD)])
+            dist_old = tfd.MultivariateNormalDiag(loc=feed['old_mus'], scale_diag=[tf.exp(LOGSTD), tf.exp(LOGSTD)])
+            log_p_n = dist.log_prob(feed['actions'])
+            log_oldp_n = dist_old.log_prob(feed['actions'])
+
+            ratio_n = tf.exp(log_p_n - log_oldp_n)
+            # ratio_n = tf.exp(log_p_n) / tf.exp(log_oldp_n)
+            surrogate_loss = None
+            if use_ppo:
+                surrogate1 = ratio_n * feed['advants']
+                surrogate2 = tf.clip_by_value(ratio_n, 1 - self.epsilon, 1 + self.epsilon) * feed['advants']
+                surrogate_loss = tf.reduce_mean(tf.math.minimum(surrogate1, surrogate2))
+            else: surrogate_loss = tf.reduce_mean(ratio_n * feed['advants'])
+        
+        return ((surrogate_loss, grad_tape))
+    
+    def get_loss(self, theta, feed):
+        # set_from_flat(self.generator, theta)
+        var_list = self.model.trainable_weights
+        shapes = [v.shape for v in var_list]
+        start = 0
+
+        weight_idx = 0
+        for shape in shapes:
+            size = np.prod(shape)
+            self.model.trainable_weights[weight_idx].assign(tf.reshape(theta[start:start + size], shape))
+            weight_idx += 1
+            start += size
+        return self.__generator_loss(feed)
+    
+    def fisher_vector_product(self, p, feed, cg_damping=0.1):
+        N = feed['states'].shape[0]
+        Nf = tf.cast(N, tf.float32)
+        var_list = self.model.trainable_weights
+
+        start = 0
+        tangents = []
+        for v in var_list:
+            size = np.prod(v.shape)
+            param = tf.reshape(p[start:(start+size)], v.shape)
+            tangents.append(param)
+            start += size
+
+        with tf.GradientTape() as tape_gvp:
+            tape_gvp.watch(var_list)
+            with tf.GradientTape() as grad_tape:
+                actions_mu = self.model([feed['states'], feed['codes']], training=True)
+                kl_firstfixed = gauss_selfKL_firstfixed(actions_mu, LOGSTD) / Nf
+
+            grads = grad_tape.gradient(kl_firstfixed, var_list)
+            gvp = [tf.reduce_sum(g * t) for (g, t) in zip(grads, tangents)]
+
+        fvp = flatgrad(self.model, gvp, tape_gvp)
+        nans = tf.math.is_nan(fvp)
+        if(tf.where(nans).numpy().flatten().shape[0] != 0): print('Fisher vector: NAN!!!!!!!!!!!')
+
+        return fvp + p * cg_damping
+    
+    def train(self, feed):
+        if use_ppo:
+            gen_optimizer = tf.keras.optimizers.Adam(learning_rate=0.005)
+
+            # for _, (states_batch, actions_batch, oldactions_batch, codes_batch, advants_batch) in enumerate(dataset):
+            for _ in range(3):
+                (surrogate_loss, grad_tape) = self.__generator_loss(feed, use_ppo)
+                gen_grads = grad_tape.gradient(surrogate_loss, self.model.trainable_weights)
+                gen_optimizer.apply_gradients(zip(gen_grads, self.model.trainable_weights))
+        
+        else:
+            # calculate previous theta (θold)
+            thprev = get_flat(self.model)
+
+            (surrogate_loss, grad_tape) = self.__generator_loss(feed)
+            policy_gradient = flatgrad(self.model, surrogate_loss, grad_tape)
+            nans = tf.math.is_nan(policy_gradient)
+            if(tf.where(nans).numpy().flatten().shape[0] != 0): print('NAN!!!!!!!!!!!')
+            stepdir = conjugate_gradient(self.fisher_vector_product, feed, policy_gradient.numpy())
+            shs = stepdir.dot(self.fisher_vector_product(stepdir, feed))
+            assert shs > 0
+
+            lm = np.sqrt(shs / self.max_kl)
+            fullstep = stepdir / lm
+            neggdotstepdir = policy_gradient.numpy().dot(stepdir)
+
+            theta = linesearch(self.get_loss, thprev, feed, fullstep, neggdotstepdir / lm)
+            # set_from_flat(self.generator, theta)
+            var_list = self.model.trainable_weights
+            shapes = [v.shape for v in var_list]
+            start = 0
+
+            weight_idx = 0
+            for shape in shapes:
+                size = np.prod(shape)
+                self.model.trainable_weights[weight_idx].assign(tf.reshape(theta[start:start + size], shape))
+                weight_idx += 1
+                start += size
+        
+        (surrogate_loss, _) = self.__generator_loss(feed)
+        episode_loss = tf.get_static_value(surrogate_loss)
+        episode_loss = episode_loss.item()
+
+        return episode_loss
+
+class Discriminator():
+    def __init__(self, state_dims, action_dims):
+        self.state_dims = state_dims
+        self.action_dims = action_dims
+        self.model = self.create_discriminator()
+        self.disc_optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
+    
+    def create_discriminator(self):
+        initializer = tf.keras.initializers.RandomNormal()
+        states = Input(shape=self.state_dims)
+        actions = Input(shape=self.action_dims)
+        merged = tf.concat([states,actions], 1)
+        x = Dense(128, kernel_initializer=initializer)(merged)
+        x = LeakyReLU()(x)
+        x = Dense(128, kernel_initializer=initializer)(x)
+        x = LeakyReLU()(x)
+        score = Dense(1)(x)
+
+        model = Model(inputs=[states, actions], outputs=score)
+        return model
+    
+    def __disc_loss(self, score1, score2):
+        cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+        genloss = cross_entropy(tf.ones_like(score1), score1)
+        expertloss = cross_entropy(tf.zeros_like(score2), score2)
+        loss = tf.reduce_mean(genloss) + tf.reduce_mean(expertloss)
+
+        return loss
+    
+    def train(self, dataset):
+        loss = 0.0
+        total_train_size = sum([el[0].shape[0] for el in list(dataset.as_numpy_iterator())])
+        for _, (generated_states_batch, generated_actions_batch, expert_states_batch, expert_actions_batch) in enumerate(dataset):
+            with tf.GradientTape() as disc_tape:
+                score1 = self.model([generated_states_batch, generated_actions_batch], training=True)
+                score2 = self.model([expert_states_batch, expert_actions_batch], training=True)
+
+                # wasserstein loss: D(x) - D(G(z))
+                # score1 = tf.reduce_mean(score1)
+                # score2 = -score2
+                # score2 = tf.reduce_mean(score2)
+                # disc_loss = tf.math.add(score1, score2)
+
+                # cross entropy loss: D(G(z)) + (1 - D(x))
+                disc_loss = self.__disc_loss(score1, score2)
+            
+            if save_loss: loss += tf.get_static_value(disc_loss) * generated_states_batch.shape[0]
+            gradients_of_discriminator = disc_tape.gradient(disc_loss, self.model.trainable_weights)
+            self.disc_optimizer.apply_gradients(zip(gradients_of_discriminator, self.model.trainable_weights))
+
+            # for l in self.discriminator.layers:
+            #     weights = l.get_weights()
+            #     weights = [np.clip(w, -0.01, 0.01) for w in weights]
+            #     l.set_weights(weights)
+        
+        episode_loss = loss / total_train_size
+        episode_loss = episode_loss.item()
+
+        return episode_loss
+
+class Posterior():
+    def __init__(self, state_dims, action_dims, code_dims):
+        self.state_dims = state_dims
+        self.action_dims = action_dims
+        self.code_dims = code_dims
+        self.model = self.create_posterior()
+        self.posterior_optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
+    
+    def create_posterior(self):
+        initializer = tf.keras.initializers.RandomNormal()
+        states = Input(shape=self.state_dims)
+        actions = Input(shape=self.action_dims)
+        merged = tf.concat([states,actions], 1)
+        x = Dense(128, kernel_initializer=initializer)(merged)
+        x = LeakyReLU()(x)
+        x = Dense(128, kernel_initializer=initializer)(x)
+        x = LeakyReLU()(x)
+        x = Dense(self.code_dims)(x)
+        output = tf.keras.activations.softmax(x)
+
+        model = Model(inputs=[states, actions], outputs=output)
+        return model
+    
+    def __post_loss(self, prob, codes_batch):
+        cross_entropy = tf.keras.losses.CategoricalCrossentropy()
+
+        loss = cross_entropy(codes_batch, prob)
+        loss = tf.reduce_mean(loss)
+
+        return loss
+    
+    def train(self, dataset):
+        loss = 0.0
+        total_train_size = sum([el[0].shape[0] for el in list(dataset.as_numpy_iterator())])
+        for _, (states_batch, actions_batch, codes_batch) in enumerate(dataset):
+            with tf.GradientTape() as post_tape:
+                prob = self.model([states_batch, actions_batch], training=True)
+
+                post_loss = self.__post_loss(prob, codes_batch)
+            
+            if save_loss: loss += tf.get_static_value(post_loss) * states_batch.shape[0]
+            gradients_of_posterior = post_tape.gradient(post_loss, self.model.trainable_weights)
+            self.posterior_optimizer.apply_gradients(zip(gradients_of_posterior, self.model.trainable_weights))
+        
+        episode_loss = loss / total_train_size
+        episode_loss = episode_loss.item()
+
+        return episode_loss
+
+class ValueNet():
+    def __init__(self, state_dims, code_dims):
+        self.state_dims = state_dims
+        self.code_dims = code_dims
+        self.model = self.create_valuenet()
+        self.value_optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
+
+    def create_valuenet(self):
+        initializer = tf.keras.initializers.RandomNormal()
+        states = Input(shape=self.state_dims)
+        codes = Input(shape=self.code_dims)
+        merged = tf.concat([states,codes], 1)
+        x = Dense(128, kernel_initializer=initializer)(merged)
+        x = LeakyReLU()(x)
+        x = Dense(128, kernel_initializer=initializer)(x)
+        x = LeakyReLU()(x)
+        output = Dense(1)(x)
+
+        model = Model(inputs=[states, codes], outputs=output)
+        return model
+    
+    def __value_loss(self, pred, returns):
+        mse = tf.keras.losses.MeanSquaredError()
+
+        loss = mse(returns, pred)
+
+        return loss
+    
+    def train(self, dataset):
+        loss = 0.0
+        total_train_size = sum([el[0].shape[0] for el in list(dataset.as_numpy_iterator())])
+        for _, (states_batch, codes_batch, returns_batch) in enumerate(dataset):
+            with tf.GradientTape() as value_tape:
+                value_pred = self.model([states_batch, codes_batch], training=True)
+
+                value_loss = self.__value_loss(value_pred, returns_batch)
+            
+            if save_loss: loss += tf.get_static_value(value_loss) * states_batch.shape[0]
+            value_grads = value_tape.gradient(value_loss, self.model.trainable_weights)
+            self.value_optimizer.apply_gradients(zip(value_grads, self.model.trainable_weights))
+        
+        episode_loss = loss / total_train_size
+        episode_loss = episode_loss.item()
+
+        return loss
+
+class Models():
+    def __init__(self, state_dims=10, action_dims=2, code_dims=3):
+        self.generator = Generator(state_dims, action_dims, code_dims)
+        self.discriminator = Discriminator(state_dims, action_dims)
+        self.posterior = Posterior(state_dims, action_dims, code_dims)
+        self.value_net = ValueNet(state_dims, code_dims)
