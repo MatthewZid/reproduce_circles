fixed action distributions and added action mu normalization

infogail.py
@@ -27,8 +27,6 @@ class Agent():
         c_traj = []
         env = CircleEnv()
 
-        logstd = np.array([LOGSTD, LOGSTD])
-
         # generate actions for every current state
         state_obsrv = env.reset() # reset environment state
         code_tf = tf.constant(code)
@@ -40,20 +38,14 @@ class Agent():
             state_tf = tf.expand_dims(state_tf, axis=0)
             action_mu = models.generator.model([state_tf, code_tf], training=False)
             action_mu = tf.squeeze(action_mu).numpy()
-            action_std = np.exp(logstd)
-
-            # sample action
-            z = np.random.randn(1, logstd.shape[0])
-            action = action_mu + action_std * z[0]
-            # action = np.clip(action, -1, 1)
 
             # current_state = (state_obsrv[-2], state_obsrv[-1])
             s_traj.append(state_obsrv)
-            a_traj.append(action)
+            a_traj.append(action_mu)
             c_traj.append(code)
 
             # 2. environment step
-            state_obsrv, done = env.step(action)
+            state_obsrv, done = env.step(action_mu)
 
             if done:
                 s_traj = np.array(s_traj, dtype=np.float32)
@@ -72,7 +64,7 @@ class Agent():
         return trajectory_dict
 
 class InfoGAIL():
-    def __init__(self, batch_size=2048, code_batch=384, episodes=6000, gamma=0.997, lam=0.97):
+    def __init__(self, batch_size=2048, code_batch=384, episodes=10000, gamma=0.997, lam=0.97):
         self.batch = batch_size
         self.code_batch = code_batch
         self.episodes = episodes
@@ -129,7 +121,7 @@ class InfoGAIL():
     def show_loss(self):
         epoch_space = np.arange(1, len(self.gen_result)+1, dtype=int)
         plt.figure()
-        plt.ylim(-100, 100)
+        # plt.ylim(-100, 100)
         plt.title('Surrogate loss')
         plt.plot(epoch_space, self.gen_result)
         plt.savefig('./plots/trpo_loss', dpi=100)
@@ -163,7 +155,7 @@ class InfoGAIL():
                 trajectories = pool.map(agent.run, sampled_codes)
             
             for traj in trajectories:
-                traj['old_action_mus'] = models.generator.model([traj['states'], traj['codes']], training=False)
+                traj['old_action_mus'] = normalize_mu(models.generator.model([traj['states'], traj['codes']], training=False))
             
             generated_states = np.concatenate([traj['states'] for traj in trajectories])
             generated_actions = np.concatenate([traj['actions'] for traj in trajectories])
@@ -227,7 +219,7 @@ class InfoGAIL():
                 traj['returns'] = discount(traj['rewards'], self.gamma)
             
             advants = np.concatenate([traj['advants'] for traj in trajectories])
-            advants = (advants - advants.mean()) / advants.std()
+            advants /= advants.std()
 
             # train value net for next iter
             returns = np.expand_dims(np.concatenate([traj['returns'] for traj in trajectories]), axis=1)

models.py
@@ -7,6 +7,7 @@ import tensorflow_probability as tfp
 from tensorflow.python.keras.layers import Input, Dense, LeakyReLU, Add
 from tensorflow.python.keras.models import Model
 import numpy as np
+import matplotlib.pyplot as plt
 from trpo import *
 
 tfd = tfp.distributions
@@ -37,14 +38,14 @@ class Generator():
 
     def __generator_loss(self, feed):
         # calculate ratio between old and new policy (surrogate loss)
-        with tf.GradientTape(persistent=True) as grad_tape:
-            actions_mu = self.model([feed['states'], feed['codes']], training=True)
+        with tf.GradientTape() as grad_tape:
+            actions_mu = normalize_mu(self.model([feed['states'], feed['codes']], training=True))
 
             nans = tf.math.is_nan(actions_mu)
             if(tf.where(nans).numpy().flatten().shape[0] != 0): print('Mus: NAN!!!!!!!!!!!')
 
             # log_p_n = gauss_log_prob(actions_mu, LOGSTD, feed['actions'])
-            # log_oldp_n = gauss_log_prob(feed['old_actions'], LOGSTD, feed['actions'])
+            # log_oldp_n = gauss_log_prob(feed['old_mus'], LOGSTD, feed['actions'])
             # ...OR...
             dist = tfd.MultivariateNormalDiag(loc=actions_mu, scale_diag=[tf.exp(LOGSTD), tf.exp(LOGSTD)])
             dist_old = tfd.MultivariateNormalDiag(loc=feed['old_mus'], scale_diag=[tf.exp(LOGSTD), tf.exp(LOGSTD)])
@@ -52,7 +53,6 @@ class Generator():
             log_oldp_n = dist_old.log_prob(feed['actions'])
 
             ratio_n = tf.exp(log_p_n - log_oldp_n)
-            # ratio_n = tf.exp(log_p_n) / tf.exp(log_oldp_n)
             surrogate_loss = None
             if use_ppo:
                 surrogate1 = ratio_n * feed['advants']
@@ -71,10 +71,17 @@ class Generator():
         weight_idx = 0
         for shape in shapes:
             size = np.prod(shape)
-            self.model.trainable_weights[weight_idx].assign(tf.reshape(theta[start:start + size], shape))
+            self.model.trainable_weights[weight_idx].assign(tf.reshape(theta[start:(start + size)], shape))
             weight_idx += 1
             start += size
         return self.__generator_loss(feed)
+    
+    def plot_gradients(self, g):
+        space = np.arange(1, g.numpy().size+1, dtype=int)
+        plt.figure()
+        plt.plot(space, g.numpy())
+        plt.savefig('./plots/gradients', dpi=100)
+        plt.close()
     
     def fisher_vector_product(self, p, feed, cg_damping=0.1):
         N = feed['states'].shape[0]
@@ -92,7 +99,7 @@ class Generator():
         with tf.GradientTape() as tape_gvp:
             tape_gvp.watch(var_list)
             with tf.GradientTape() as grad_tape:
-                actions_mu = self.model([feed['states'], feed['codes']], training=True)
+                actions_mu = normalize_mu(self.model([feed['states'], feed['codes']], training=True))
                 kl_firstfixed = gauss_selfKL_firstfixed(actions_mu, LOGSTD) / Nf
 
             grads = grad_tape.gradient(kl_firstfixed, var_list)
@@ -139,7 +146,7 @@ class Generator():
             weight_idx = 0
             for shape in shapes:
                 size = np.prod(shape)
-                self.model.trainable_weights[weight_idx].assign(tf.reshape(theta[start:start + size], shape))
+                self.model.trainable_weights[weight_idx].assign(tf.reshape(theta[start:(start + size)], shape))
                 weight_idx += 1
                 start += size
         
@@ -276,6 +283,7 @@ class ValueNet():
         x = Dense(128, kernel_initializer=initializer)(x)
         x = LeakyReLU()(x)
         output = Dense(1)(x)
+        # output = tf.keras.activations.sigmoid(output)
 
         model = Model(inputs=[states, codes], outputs=output)
         return model
@@ -303,7 +311,7 @@ class ValueNet():
         episode_loss = loss / total_train_size
         episode_loss = episode_loss.item()
 
-        return loss
+        return episode_loss
 
 class Models():
     def __init__(self, state_dims=10, action_dims=2, code_dims=3):

trpo.py
@@ -6,10 +6,11 @@ import tensorflow as tf
 
 improved = 0
 save_loss = True
-save_models = False
+save_models = True
 resume_training = False
 use_ppo = False
-LOGSTD = -3.5
+LOGSTD = tf.math.log(0.005)
+SPEED = 0.02
 
 def discount(x, gamma):
     assert x.ndim >= 1
@@ -64,6 +65,19 @@ def gauss_KL(mu1, logstd1, mu2, logstd2):
     kl = tf.reduce_sum(logstd2 - logstd1 + (var1 + tf.square(mu1 - mu2))/(2*var2) - 0.5)
     return kl
 
+def normalize_mu(action_mu):
+    norm = tf.math.sqrt(tf.math.add(tf.math.pow(tf.gather(action_mu, 0, axis=1),2), tf.math.pow(tf.gather(action_mu, 1, axis=1),2)))
+    norm = tf.expand_dims(norm, 1)
+
+    if tf.where(tf.math.greater(norm, 1e-8)).shape[0] != 0:
+        action_mu /= norm
+    else:
+        action_mu = tf.concat([tf.ones((action_mu.shape[0],1)), tf.zeros((action_mu.shape[0],1))], 1)
+    
+    action_mu *= SPEED
+
+    return action_mu
+
 def linesearch(f, x, feed, fullstep, expected_improve_rate):
     accept_ratio = .1
     max_backtracks = 10
@@ -72,13 +86,13 @@ def linesearch(f, x, feed, fullstep, expected_improve_rate):
         xnew = x + stepfrac * fullstep
         newfval = f(xnew, feed)[0]
         actual_improve = fval - newfval
+        # actual_improve = newfval - fval
         expected_improve = expected_improve_rate * stepfrac
         ratio = actual_improve / expected_improve
         if ratio > accept_ratio and actual_improve > 0:
             global improved
             improved += 1
             return xnew
-    # print("NOT IMPROVED............")
     return x
 
 def conjugate_gradient(f_Ax, feed, b, cg_iters=10, residual_tol=1e-10):